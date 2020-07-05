80216a26bae11e1c17927eeee79f294e4edb236a
odilonjk <kroger.odilonj@gmail.com>
2020-07-05 19:16:28 -0300

    moved products data to data.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import data from './data';
 import './App.css';
 
 const openMenu = () => {
@@ -35,72 +36,20 @@ function App() {
       <main className="main">
         <div className="content">
           <ul className="products">
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/sophies_world.jpg" alt="Book 1"/>
-                <div className="product-details">
-                  <div className="product-name"><a href="product.html">Sophie's World</a></div>
-                  <div className="product-author">Jostein Gaarder</div>
-                  <div className="product-price">$22.90</div>
-                  <div className="product-rating">4.5 start (331 reviews)</div>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/meditations.jpg" alt="Book 2"/>
-                <div className="product-details">
-                  <div className="product-name"><a href="product.html">Meditations</a></div>
-                  <div className="product-author">Marcus Aurelius</div>
-                  <div className="product-price">$14.50</div>
-                  <div className="product-rating">5 stars (729 reviews)</div>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/letters_from_a_stoic.jpg" alt="Book 3"/>
-                <div className="product-details">
-                  <div className="product-name"><a href="product.html">Letters From A Stoic</a></div>
-                  <div className="product-author">Seneca</div>
-                  <div className="product-price">$16.00</div>
-                  <div className="product-rating">5 stars (642 reviews)</div>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/beyond_good_and_evil.jpg" alt="Book 4"/>
-                <div className="product-details">
-                  <div className="product-name"><a href="product.name">Beyond Good and Evil</a></div>
-                  <div className="product-author">Friedrich Nietzsche</div>
-                  <div className="product-price">$18.00</div>
-                  <div className="product-rating">4.5 stars (495 reviews)</div>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/the_antichrist.jpg" alt="Book 5"/>
-                <div className="product-details">
-                  <div className="product-name"><a href="product.html">The Anti-christ</a></div>
-                  <div className="product-author">Friedrich Nietzsche</div>
-                  <div className="product-price">$16.00</div>
-                  <div className="product-rating">4.0 stars (17 reviews)</div>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/the_myth_of_sisyphus.jpg" alt="Book 6"/>
-                <div className="product-details">
-                  <div className="product-name"><a href="product.html">The Myth of Sisyphus</a></div>
-                  <div className="product-author">Albert Camus</div>
-                  <div className="product-price">$22.50</div>
-                  <div className="product-rating">4.5 stars (251 reviews)</div>
-                </div>
-              </div>
-            </li>  
+            {
+              data.products.map(product =>
+                <li>
+                  <div className="product">
+                    <img className="product-image" src={product.image} alt={product.name}/>
+                    <div className="product-details">
+                      <div className="product-name"><a href="product.html">{product.name}</a></div>
+                      <div className="product-author">{product.author}</div>
+                      <div className="product-price">${product.price}</div>
+                      <div className="product-rating">{product.rating} stars ({product.totalReviews} reviews)</div>
+                    </div>
+                  </div>
+                </li>)
+            }  
           </ul>
         </div>
       </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+export default {
+    products: [
+        {
+            name: "Sophie's World",
+            image: "/images/sophies_world.jpg",
+            author: "Jostein Gaarder",
+            price: 22.90,
+            rating: 4.5,
+            totalReviews: 331,
+            category: "Philosophy"
+        },
+        {
+            name: "Meditations",
+            image: "/images/meditations.jpg",
+            author: "Marcus Aurelius",
+            price: 14.50,
+            rating: 5,
+            totalReviews: 729,
+            category: "Philosophy"
+        },
+        {
+            name: "Letters From A Stoic",
+            image: "/images/letters_from_a_stoic.jpg",
+            author: "Seneca",
+            price: 16.00,
+            rating: 5,
+            totalReviews: 642,
+            category: "Philosophy"
+        },
+        {
+            name: "Beyond Good and Evil",
+            image: "/images/beyond_good_and_evil.jpg",
+            author: "Friedrich Nietzsche",
+            price: 18.00,
+            rating: 4.5,
+            totalReviews: 495,
+            category: "Philosophy"
+        },
+        {
+            name: "The Anti-christ",
+            image: "/images/the_antichrist.jpg",
+            author: "Friedrich Nietzsche",
+            price: 16.90,
+            rating: 4.0,
+            totalReviews: 130,
+            category: "Philosophy"
+        },
+        {
+            name: "The Myth of Sisyphus",
+            image: "/images/the_myth_of_sisyphus.jpg",
+            author: "Albert Camus",
+            price: 22.50,
+            rating: 4.5,
+            totalReviews: 251,
+            category: "Philosophy"
+        },
+        {
+            name: "Think Python",
+            image: "/images/think_python.jpg",
+            author: "Allen B. Downey",
+            price: 80.50,
+            rating: 4.5,
+            totalReviews: 101,
+            category: "Technology"
+        },
+        {
+            name: "Pro Git",
+            image: "/images/pro_git.jpg",
+            author: "Scott Chacon, Ben Straub",
+            price: 96.90,
+            rating: 4.5,
+            totalReviews: 272,
+            category: "Technology"
+        }
+    ]
+}
